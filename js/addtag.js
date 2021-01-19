f75fe308d1323c4b45e93999d1c163f65288181f
Brianmuvel <brianmuvel@gmail.com>
2021-01-19 17:07:42 -0600

    funcionalidad de javascrip

---
+++ js/addtag.js
@@ -0,0 +1,125 @@
+
+let allTags = []
+const getTags = () => {
+    $.ajax("https://pruebas-frontend-default-rtdb.firebaseio.com/tags/.json")
+        .done(function (tags) {
+            for (item in tags) {
+                allTags.push(tags[item].tagname)
+            }
+        })
+        .fail(function (err) {
+            alert("error", err);
+        })
+}
+
+
+
+$(document).ready(function () {
+    // obtener los tags existentes
+    // guardarlos en una variable
+    //console.log(allTags)
+    //["mytag", mytag 2", mytag 3"]
+    // escuchar el evento de crear
+    $('.send__tag').click(function () {
+
+        let tagValue = $('#tag__input').val().toLowerCase()
+        if (tagValue === "") {
+            $('.alert__tag').text('Ingresa un tag').slideDown()
+            return false
+            //allTags.includes(tagText) === false
+        }
+        if (allTags.includes(tagValue.toLowerCase()) === true) {
+            // comprobar si existe o no el tag
+            $('.alert__tag').text('El tag ya existe, intenta con otro').slideDown()
+            $('#form-new-tag').trigger('reset')
+            // si existe, mandar mensaje
+        } else {
+            // sino, hacer el POST
+            let tagObject = {
+                tagname: tagValue.toLowerCase()
+            }
+            $.ajax({
+                url: "https://pruebas-frontend-default-rtdb.firebaseio.com/tags/.json",
+                method: 'POST',
+                data: JSON.stringify(tagObject)
+            }).done(function (response) {
+                $('.alert__tag').text('Tag creado').slideDown()
+                getTags()
+            }).fail(function (err) {
+                console.log(err)
+                $('.alert__tag').text('No se pudo crear el tag').slideDown()
+            })
+        }
+        
+    })
+    getTags()
+})
+$('#form-new-tag').on('click', '.clear__tag', function () {
+    $('#form-new-tag').trigger('reset')
+})
+
+
+
+//$('#lista__tags').html(listaCreados)
+/* //DELETE
+$.ajax({
+    url: "https://pruebas-frontend-default-rtdb.firebaseio.com/tags/.json",
+    method: 'DELETE'
+}).done(function(response) {
+    //$('.alert__tag').text('Tag creado').slideDown()
+}).fail(function(err){
+    console.log(err)
+    //$('.alert__tag').text('No se pudo crear el tag').slideDown()
+}) */
+
+
+/* 
+//VERSION DE JORGE
+let allTags = []
+const getTags = () => {
+    $.ajax( "https://" )
+    .done(function(res) {
+        allTags = []
+        for(item in res) {
+            allTags.push(res[item].title)
+        }
+        console.log(allTags)
+        //["mytag", mytag 2", mytag 3"]
+    })
+    .fail(function(err) {
+        alert( "error", err);
+    })
+}
+$(document).ready(function() {
+    getTags()
+    $('.btn__save__tags').click(function(){
+        let tagText = $('.input__tag').val()
+        // si esta vacio
+        if (tagText === "") {
+            $('.alert__tag').text('Ingresa un tag').slideDown()
+            return false
+        }
+        // sino esta vacio 
+        if ( allTags.includes(tagText) ) {
+            // comprobar si existe o no el tag
+            $('.alert__tag').text('El tag ya existe').slideDown()
+            // si existe, mandar mensaje
+        } else {
+            // sino, hacer el POST
+            let tagObject = {
+                title: tagText
+            }
+            $.ajax({
+                url: "https://",
+                method: 'POST',
+                data: JSON.stringify(tagObject)
+            }).done(function(response) {
+                $('.alert__tag').text('Tag creado').slideDown()
+            }).fail(function(err){
+                console.log(err)
+                $('.alert__tag').text('No se pudo crear el tag').slideDown()
+            })
+        }
+    })
+})
+ */
---
+++ js/newpost.js
@@ -0,0 +1,54 @@
+
+function clearForm() {
+    $('#form-new-post').trigger('reset')
+}
+
+$('#form-new-post').on('click','.send__post', function() {
+    let today = new Date().toLocaleDateString()
+    let postObject = {
+        title: $('#title__input').val(),
+        content: $('#content__input').val(),
+        urlCover: $('#urlCover__input').val(),
+        tag: $('#taglist__select').val(),
+        author: $('#author__input').val(),
+        minsToRead: $('#time__input').val(),
+        date: today,
+    }
+    let validate = postObject.title.length > 0 && postObject.content.length > 0 && postObject.urlCover.length > 0 && postObject.tag.length > 0 && postObject.urlCover.length > 0 && postObject.tag.length > 0 && postObject.author.length > 0 && postObject.minsToRead.length > 0
+    if(validate){
+
+        $.ajax({
+            url: "https://pruebas-frontend-default-rtdb.firebaseio.com/post/.json",
+            method: 'POST',
+            data: JSON.stringify(postObject)
+        }).done(function() {
+            
+            //console.log(response)
+        }).fail(function(err){
+            console.log(err)
+        })
+        $('#alert__form').removeClass('d-none')
+        $('#alert').addClass('d-none')
+        clearForm()
+
+    }else {
+        $('#alert').css('position', 'relative')
+        $('#alert').addClass('d-block')
+        console.log('no se puede')
+    }
+})
+
+$('#form-new-post').on('click','.clear__post', function() {
+    clearForm()
+})
+
+/* //delete
+let idPost = "aqui el id del post a borrar"
+$.ajax({
+    url: "https://pruebas-frontend-default-rtdb.firebaseio.com/post/-MRNyLYtUWkoD96xUXPs.json",
+    method: 'DELETE'
+}).done(function(response) {
+   console.log('Se ha eliminado')
+}).fail(function(err){
+    console.log(err)
+}) */
